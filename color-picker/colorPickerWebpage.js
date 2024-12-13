f5a6b5fdccaf9230c91228ca6ce1aa4fa58c9f70
NagaraniKanchi <nagakanchi22@gmail.com>
2024-12-13 10:28:07 +0530

    here color-picker code

---
+++ color-picker/colorPickerWebpage.js
@@ -0,0 +1,21 @@
+
+let colorPickerContainerElement = document.getElementById("colorPickerContainerElement");
+let colorHexaCode = document.getElementById("colorHexaCode");
+
+function changeBgColorToGrayAndUpdateText() {
+    colorPickerContainerElement.style.backgroundColor = '#e0e0e0';
+    colorHexaCode.textContent = " #e0e0e0"
+}
+function changeBgColorToGreenAndUpdateText() {
+    colorPickerContainerElement.style.backgroundColor = '#6fcf97';
+    colorHexaCode.textContent = " #6fcf97"
+}
+function changeBgColorToBlueAndUbdateText() {
+    colorPickerContainerElement.style.backgroundColor = '#56ccf2';
+    colorHexaCode.textContent = " #56ccf2"
+}
+function changeBgColorToVoiletAndUpdateText() {
+    colorPickerContainerElement.style.backgroundColor = '#bb6bd9';
+    colorHexaCode.textContent = " #bb6bd9";
+
+}
